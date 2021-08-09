3c485a35f62812b7e08f8c34b637802d422bc4f3
Burak <cccswagboyyolo@yahoo.com>
2021-08-09 15:51:02 +0300

    refactor: example app's visual design

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,32 @@
-import * as React from 'react';
+import React from 'react';
 
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import { useBottomModal, BottomModal } from 'react-native-lightning-modal';
 
 export default function App() {
   const { dismiss, show, modalProps } = useBottomModal();
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={show}>
-        <Text>show</Text>
+      <StatusBar translucent backgroundColor="transparent" />
+      <Text style={[styles.headingText, styles.header]}>Hi.👋</Text>
+      <TouchableOpacity style={styles.button} onPress={show}>
+        <Text style={styles.buttonText}>Show a lightning modal! ⚡</Text>
       </TouchableOpacity>
-      <BottomModal backdropColor="rgba(0,0,0,0.5)" height={500} {...modalProps}>
-        <TouchableOpacity onPress={dismiss}>
-          <Text>dismiss</Text>
-        </TouchableOpacity>
+      <View style={styles.fill} />
+      <BottomModal backdropColor="rgba(0,0,0,0.5)" height={300} {...modalProps}>
+        <View style={styles.modalContainer}>
+          <Text style={[styles.headingText, styles.header]}>I’m alive!</Text>
+          <TouchableOpacity style={styles.buttonSecondary} onPress={dismiss}>
+            <Text style={styles.buttonText}>Hide me ⛅</Text>
+          </TouchableOpacity>
+        </View>
       </BottomModal>
     </View>
   );
@@ -24,5 +37,32 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  headingText: {
+    fontWeight: '700',
+    color: 'black',
+    fontSize: 50,
+  },
+  header: { marginBottom: 30 },
+  fill: { flex: 1 / 6 },
+  button: {
+    backgroundColor: '#F7FFDF',
+    borderRadius: 10,
+    padding: 6,
+  },
+  buttonSecondary: {
+    backgroundColor: '#D9F6FF',
+    borderRadius: 10,
+    padding: 8,
+  },
+  buttonText: {
+    fontSize: 14,
+    color: 'black',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
